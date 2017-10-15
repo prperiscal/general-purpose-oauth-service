3fdff53a81b06c1636f68ac33cf87e3291f9b9d8
prperiscal <prperiscal@gmail.com>
2017-10-15 20:41:45 +0200

    new(project): OAUTH service first commit

---
+++ src/main/java/com/myfashion/myfashionoauthservice/MyfashionOauthServiceApplication.java
@@ -0,0 +1,12 @@
+package com.myfashion.myfashionoauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfashionOauthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyfashionOauthServiceApplication.class, args);
+    }
+}
